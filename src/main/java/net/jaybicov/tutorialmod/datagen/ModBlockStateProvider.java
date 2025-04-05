9135978f856469eb58f3b2b5947a6cf0c4c05607
Semi Eren Gokyildiz <127510869+SERENGOKYILDIZ@users.noreply.github.com>
2025-04-06 02:50:02 +0300

    Added Datagen Setup. (Now JSON files will be generated automatically.)

---
+++ src/main/java/net/jaybicov/tutorialmod/datagen/ModBlockStateProvider.java
@@ -0,0 +1,30 @@
+package net.jaybicov.tutorialmod.datagen;
+
+import net.jaybicov.tutorialmod.TutorialMod;
+import net.jaybicov.tutorialmod.block.ModBlocks;
+import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModBlockStateProvider extends BlockStateProvider {
+    public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
+        super(output, TutorialMod.MOD_ID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+        blockWithItem(ModBlocks.ALEXANDRITE_BLOCK);
+        blockWithItem(ModBlocks.RAW_ALEXANDRITE_BLOCK);
+        blockWithItem(ModBlocks.ALEXANDRITE_ORE);
+        blockWithItem(ModBlocks.ALEXANDRITE_DEEPSLATE_ORE);
+        blockWithItem(ModBlocks.MAGIC_BLOCK);
+    }
+
+    // This method makes the blocks we send into both an item and defines that they are square.
+    private void blockWithItem(RegistryObject<Block> blockRegistryObject)
+    {
+        simpleBlockWithItem(blockRegistryObject.get(), cubeAll(blockRegistryObject.get()));
+    }
+}
