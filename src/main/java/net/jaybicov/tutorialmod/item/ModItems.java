9a94842602a380cfdcf943bc0edce4aa5fc234cc
Semi Eren Gokyildiz <127510869+SERENGOKYILDIZ@users.noreply.github.com>
2025-04-05 03:52:35 +0300

    Added Custom Tooltips

--- src/main/java/net/jaybicov/tutorialmod/item/ModItems.java
+++ src/main/java/net/jaybicov/tutorialmod/item/ModItems.java
@@ -3,12 +3,17 @@ package net.jaybicov.tutorialmod.item;
 import net.jaybicov.tutorialmod.TutorialMod;
 import net.jaybicov.tutorialmod.item.custom.ChiselItem;
 import net.jaybicov.tutorialmod.item.custom.FuelItem;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+
+import java.util.List;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
@@ -34,7 +39,15 @@ public class ModItems {
     public static final RegistryObject<Item> KOHLRABI = ITEMS.register("kohlrabi",
             ()-> new Item(new Item.Properties()
                     .food(ModFoodProperties.KOHLRABI)
-            ));
+            )
+                // Adding a custom tooltip with an anonymous class
+            {
+                @Override
+                public void appendHoverText(ItemStack pStack, TooltipContext pContext, List<Component> pTooltipComponents, TooltipFlag pTooltipFlag) {
+                    pTooltipComponents.add(Component.translatable("tooltip.tutorialmod.kohlrabi"));
+                    super.appendHoverText(pStack, pContext, pTooltipComponents, pTooltipFlag);
+                }
+            });
 
     //For Custom Fuel item:
     public static final RegistryObject<Item> AURORA_ASHES = ITEMS.register("aurora_ashes",
